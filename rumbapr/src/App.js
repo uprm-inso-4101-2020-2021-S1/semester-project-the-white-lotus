ff5a28861cadd62df2493bfcf8863ae20d7dba53
FherRodz <fher.rodriguez@upr.edu>
2020-09-29 14:49:44 -0400

    Header TroubleShooting

--- rumbapr/src/App.js
+++ rumbapr/src/App.js
@@ -1,18 +1,3 @@
-import React from 'react';
-<<<<<<< HEAD
-import logo from './logo.svg';
-import Header from './components/header/Header.js'
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-
-
-function App() {
-  return (
-    <div className="App">
-      <Header ></Header>
-    </div>
-  );
-=======
 import './App.css';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import forYouPage from './pages/forYouPage';
@@ -34,7 +19,6 @@ class App extends React.Component {
       </BrowserRouter>
     );
   }
->>>>>>> c985b497f3b35c2b52ad4f4f80ee2ba381bc1d6b
 }
 
 export default App;
