44b30765bb217840ac9db5fac1ae3c3ea85193a0
hackerrank <me@hackerrank.com>
2020-09-30 18:44:59 -0400

    Burger menu done

--- rumbapr/src/components/HamburgerMenu/HamburgerMenu.js
+++ rumbapr/src/components/HamburgerMenu/HamburgerMenu.js
@@ -1,31 +1,79 @@
 import React from 'react';
+import './HamburgerMenu.css'
+import './HamburgerMenuDesign.css'
 
-/*const HamburgerMenu = () => {
-    return (
-        <div>
-            <link rel="stylesheet" href="./HamburgerMenu/HamburgerMenuDesign.css" />
-            <div class="menu-wrap">
-            <input 
-          type="checkbox" 
-          value="Submit" 
-          className="btn"
-          style={{flex: '1'}}
-        />
+
+// mira este tutorial https://mdbootstrap.com/docs/react/navigation/hamburger-menu/ por si quieres volver a empezar or something
+// me avisas tengo que terminar una cosas de otra clase
+// Dale, ill give it a watch. Thanks
+
+
+export class HamburgerMenu extends React.Component{
+  render(){
+    return(
+      <div className="hamburger-container">
+      <body>
+        <div className="menu-wrap" />
+          <input type="checkbox" className="toggler" />
+          <div className="hamburger"><div></div></div>
+          <div className="menu">
+            <div>
+              <div>
+                <ul>
+                  <li><a href="#">Type of location</a></li>
+                  <li><a href="#">Filters</a></li>
+                  <li><a href="#">Preffered distance</a></li>
+                  <li><a href="#">Price range</a></li>
+                </ul>
+              </div>
             </div>
-            <ul>
-                <li>Type of location</li>
-                <li>Filters</li>
-                <li>Prefferd distance</li>
-                <li>Price range</li>
-            </ul>
-        </div>
+          </div>
+        </body>
+      </div>
     )
-}*/
-
-const HamburgerMenu = () => {
-    return (
-        <input type="checkbox" id="checked" onClick="clicked"/>
-    )
+  }
 }
 
-export default HamburgerMenu
+/*
+export class HamburgerMenu extends React.Component{
+  render() {
+    return (
+      <div className="hamburger-container">
+        <head>
+          <title>Hamburger Menu Overlay</title>
+        </head>
+        <body>
+        <div class="menu-wrap" />
+          <input type="checkbox" class="toggler" />
+          <div class="hamburger"><div></div></div>
+          <div class="menu">
+            <div>
+              <div>
+                <ul>
+                  <li><a href="#">Type of location</a></li>
+                  <li><a href="#">Filters</a></li>
+                  <li><a href="#">Preffered distance</a></li>
+                  <li><a href="#">Price range</a></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </body>
+      </div>
+    )
+  }
+}
+*/
+
+/*
+export class HamburgerMenu {
+    render{
+      return (
+        <div class className='hamburger-container'>
+          <div className
+        </div>
+      )
+    }
+}
+*/
+export default HamburgerMenu;
--- rumbapr/src/pages/landingPage.js
+++ rumbapr/src/pages/landingPage.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import Button from 'react-bootstrap/Button';
+import HamburgerMenu from '../components/HamburgerMenu/HamburgerMenu';
 
 
 const landingPage = () => {
     return(
         <div>
+            <HamburgerMenu />
             <h1>Landing Page</h1>
             <p>Something something</p>
             <Button variant="secondary"><NavLink to="/foryou">For you</NavLink></Button>
