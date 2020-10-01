9507193a39698ca55cf67f6d02649647cbf1ce8d
hackerrank <me@hackerrank.com>
2020-10-01 09:42:37 -0400

    Added a base for the options inside the burgerMenu. Some of the additions are 2 sliders for price and distance range.

--- rumbapr/src/components/HamburgerMenu.js
+++ rumbapr/src/components/HamburgerMenu.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './HamburgerMenuDesigns/HamburgerMenu.css'
 import './HamburgerMenuDesigns/btnStyle.css'
+import NavDropdown from 'react-bootstrap/NavDropdown';
 
 export class HamburgerMenu extends React.Component{
   render(){
@@ -14,10 +15,20 @@ export class HamburgerMenu extends React.Component{
             <div>
               <div>
                 <ul>
-                  <li><a href="#">Type of location</a></li>
-                  <li><a href="#">Filters</a></li>
-                  <li><a href="#">Preffered distance</a></li>
-                  <li><a href="#">Price range</a></li>
+                <NavDropdown title="Type of location" id="dropdown-basic-button">
+                  <NavDropdown.Item href="#action/3.1">Food</NavDropdown.Item>
+                  <NavDropdown.Item href="#action/3.2">Hotel</NavDropdown.Item>
+                  <NavDropdown.Item href="#action/3.3">Attraction</NavDropdown.Item>
+                </NavDropdown>
+                <li><a href="#">Filters</a></li>
+                <li><a href="#">Preffered distance</a></li>
+                <div class="slidecontainer">
+                <input type="range" min="1" max="3" value="2" class="slider" id="myDistance" />
+                </div>
+                <li><a href="#">Price range</a></li>
+                <div class="slidecontainer">
+                <input type="range" min="1" max="3" value="2" class="slider" id="myPrice" />
+                </div>
                 </ul>
               </div>
             </div>
