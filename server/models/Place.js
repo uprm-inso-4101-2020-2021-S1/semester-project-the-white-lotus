f474973588a18af3e98e4771835cefdbee7031db
Jean Carlos Rivera Hernandez <jean.rivera4@upr.edu>
2020-10-05 16:41:28 -0400

    Added Filter Object.

--- server/models/Place.js
+++ server/models/Place.js
@@ -1,41 +1,41 @@
-const mongoose = require('mongoose')
-const Schema = mongoose.Schema
+const mongoose = require('mongoose');
 
+const { Schema } = mongoose;
 
 const placeSchema = new Schema({
-    name: {
-        type: String,
-        required: true
-    },
-    email: {
-        type: String,
-        required: true
-    },
-    phone: {
-        type: String,
-        required: true
-    },
-    address: {
-        type: String,
-        required: true
-    },
-    city: {
-        type: String,
-        required: true
-    },
-    country: {
-        type: String,
-        required: true
-    },
-    comments: {
-        type: Array
-    },
-    photos: {
-        type: Array
-    },
-    hashtags: {
-        type: Array
-    }
-}, {timestamps: true})
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  phone: {
+    type: String,
+    required: true
+  },
+  address: {
+    type: String,
+    required: true
+  },
+  city: {
+    type: String,
+    required: true
+  },
+  country: {
+    type: String,
+    required: true
+  },
+  comments: {
+    type: Array
+  },
+  photos: {
+    type: Array
+  },
+  hashtags: {
+    type: Array
+  }
+}, { timestamps: true });
 
-module.exports = mongoose.model('Place', placeSchema)
+module.exports = mongoose.model('Place', placeSchema);
