aacb240533e57269b96371ce5299ee0e0e30ffac
malejandramunoz <maria.munoz10@upr.edu>
2020-11-01 19:30:08 -0400

    Addition of the dummy cards, still needs work

--- rumbapr/src/pages/forYouPage.js
+++ rumbapr/src/pages/forYouPage.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import Header from '../components/header/Header';
 import { MDBMask, MDBView, MDBContainer, MDBRow, MDBCol } from "mdbreact";
+import './forYouPage.css';
 
 const forYouPage = () => {
     return(
-        <div className="for_you_container">
+        <div className="for_container">
             <div className="header_container">
                 <Header />
             </div>
-                <MDBContainer className="container_cards">
+                <MDBContainer className="mt-3">
                     <MDBRow>
-                        <MDBCol md="3">
+                        <MDBCol md="4">
                             <MDBView hover>
                                 <img src="https://mdbootstrap.com/img/Others/documentation/forest-sm-mini.jpg"
                                      className="img-fluid"
@@ -21,7 +22,7 @@ const forYouPage = () => {
                                 </MDBMask>
                             </MDBView>
                         </MDBCol>
-                        <MDBCol md="3">
+                        <MDBCol md="4">
                             <MDBView hover>
                                 <img src="https://mdbootstrap.com/img/Others/documentation/forest-sm-mini.jpg"
                                      className="img-fluid"
@@ -32,7 +33,7 @@ const forYouPage = () => {
                                 </MDBMask>
                             </MDBView>
                         </MDBCol>
-                        <MDBCol md="3">
+                        <MDBCol md="4">
                             <MDBView hover>
                                 <img src="https://mdbootstrap.com/img/Others/documentation/forest-sm-mini.jpg"
                                      className="img-fluid"
@@ -43,7 +44,104 @@ const forYouPage = () => {
                                 </MDBMask>
                             </MDBView>
                         </MDBCol>
-                        <MDBCol md="3">
+                    </MDBRow>
+                    <p />
+                    <MDBRow>
+                        <MDBCol md="4">
+                            <MDBView hover>
+                                <img src="https://mdbootstrap.com/img/Others/documentation/forest-sm-mini.jpg"
+                                     className="img-fluid"
+                                     alt=""
+                                />
+                                <MDBMask className="flex-center" overlay="teal-light">
+                                    <p className="white-text">Testing</p>
+                                </MDBMask>
+                            </MDBView>
+                        </MDBCol>
+                        <MDBCol md="4">
+                            <MDBView hover>
+                                <img src="https://mdbootstrap.com/img/Others/documentation/forest-sm-mini.jpg"
+                                     className="img-fluid"
+                                     alt=""
+                                />
+                                <MDBMask className="flex-center" overlay="teal-light">
+                                    <p className="white-text">Testing</p>
+                                </MDBMask>
+                            </MDBView>
+                        </MDBCol>
+                        <MDBCol md="4">
+                            <MDBView hover>
+                                <img src="https://mdbootstrap.com/img/Others/documentation/forest-sm-mini.jpg"
+                                     className="img-fluid"
+                                     alt=""
+                                />
+                                <MDBMask className="flex-center" overlay="teal-light">
+                                    <p className="white-text">Testing</p>
+                                </MDBMask>
+                            </MDBView>
+                        </MDBCol>
+                    </MDBRow>
+                    <p />
+                    <MDBRow>
+                        <MDBCol md="4">
+                            <MDBView hover>
+                                <img src="https://mdbootstrap.com/img/Others/documentation/forest-sm-mini.jpg"
+                                     className="img-fluid"
+                                     alt=""
+                                />
+                                <MDBMask className="flex-center" overlay="teal-light">
+                                    <p className="white-text">Testing</p>
+                                </MDBMask>
+                            </MDBView>
+                        </MDBCol>
+                        <MDBCol md="4">
+                            <MDBView hover>
+                                <img src="https://mdbootstrap.com/img/Others/documentation/forest-sm-mini.jpg"
+                                     className="img-fluid"
+                                     alt=""
+                                />
+                                <MDBMask className="flex-center" overlay="teal-light">
+                                    <p className="white-text">Testing</p>
+                                </MDBMask>
+                            </MDBView>
+                        </MDBCol>
+                        <MDBCol md="4">
+                            <MDBView hover>
+                                <img src="https://mdbootstrap.com/img/Others/documentation/forest-sm-mini.jpg"
+                                     className="img-fluid"
+                                     alt=""
+                                />
+                                <MDBMask className="flex-center" overlay="teal-light">
+                                    <p className="white-text">Testing</p>
+                                </MDBMask>
+                            </MDBView>
+                        </MDBCol>
+                    </MDBRow>
+                    <p />
+                    <MDBRow>
+                        <MDBCol md="4">
+                            <MDBView hover>
+                                <img src="https://mdbootstrap.com/img/Others/documentation/forest-sm-mini.jpg"
+                                     className="img-fluid"
+                                     alt=""
+                                />
+                                <MDBMask className="flex-center" overlay="teal-light">
+                                    <p className="white-text">Testing</p>
+                                </MDBMask>
+                            </MDBView>
+                        </MDBCol>
+                        <MDBCol md="4">
+                            <MDBView hover>
+                                <img src="https://mdbootstrap.com/img/Others/documentation/forest-sm-mini.jpg"
+                                     className="img-fluid"
+                                     alt=""
+                                />
+                                <MDBMask className="flex-center" overlay="teal-light">
+                                    <p className="white-text">Testing</p>
+                                </MDBMask>
+                            </MDBView>
+                        </MDBCol>
+                        <MDBCol md="4">
                             <MDBView hover>
                                 <img src="https://mdbootstrap.com/img/Others/documentation/forest-sm-mini.jpg"
                                      className="img-fluid"
