559984fbc99f84bcbc29a72fa4c859b20bb12354
malejandramunoz <maria.munoz10@upr.edu>
2020-11-01 18:50:43 -0400

    Addition of the mdbootstrap to the 'for you' page

--- rumbapr/src/pages/forYouPage.js
+++ rumbapr/src/pages/forYouPage.js
@@ -1,10 +1,61 @@
 import React from 'react';
 import Header from '../components/header/Header';
+import { MDBMask, MDBView, MDBContainer, MDBRow, MDBCol } from "mdbreact";
 
 const forYouPage = () => {
     return(
-        <div className="header_container">
-            <Header />
+        <div className="for_you_container">
+            <div className="header_container">
+                <Header />
+            </div>
+                <MDBContainer className="container_cards">
+                    <MDBRow>
+                        <MDBCol md="3">
+                            <MDBView hover>
+                                <img src="https://mdbootstrap.com/img/Others/documentation/forest-sm-mini.jpg"
+                                     className="img-fluid"
+                                     alt=""
+                                />
+                                <MDBMask className="flex-center" overlay="teal-light">
+                                    <p className="white-text">Testing</p>
+                                </MDBMask>
+                            </MDBView>
+                        </MDBCol>
+                        <MDBCol md="3">
+                            <MDBView hover>
+                                <img src="https://mdbootstrap.com/img/Others/documentation/forest-sm-mini.jpg"
+                                     className="img-fluid"
+                                     alt=""
+                                />
+                                <MDBMask className="flex-center" overlay="teal-light">
+                                    <p className="white-text">Testing</p>
+                                </MDBMask>
+                            </MDBView>
+                        </MDBCol>
+                        <MDBCol md="3">
+                            <MDBView hover>
+                                <img src="https://mdbootstrap.com/img/Others/documentation/forest-sm-mini.jpg"
+                                     className="img-fluid"
+                                     alt=""
+                                />
+                                <MDBMask className="flex-center" overlay="teal-light">
+                                    <p className="white-text">Testing</p>
+                                </MDBMask>
+                            </MDBView>
+                        </MDBCol>
+                        <MDBCol md="3">
+                            <MDBView hover>
+                                <img src="https://mdbootstrap.com/img/Others/documentation/forest-sm-mini.jpg"
+                                     className="img-fluid"
+                                     alt=""
+                                />
+                                <MDBMask className="flex-center" overlay="teal-light">
+                                    <p className="white-text">Testing</p>
+                                </MDBMask>
+                            </MDBView>
+                        </MDBCol>
+                    </MDBRow>
+                </MDBContainer>
         </div>
     )
 }
