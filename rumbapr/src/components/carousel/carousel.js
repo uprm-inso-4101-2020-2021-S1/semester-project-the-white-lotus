65fc2e49e16b04cd5c2ed630ec9b2308ec978d69
hackerrank <me@hackerrank.com>
2020-11-09 11:14:39 -0400

    Base for the carousel presenting images.

--- rumbapr/src/components/carousel/carousel.js
+++ rumbapr/src/components/carousel/carousel.js
@@ -2,16 +2,19 @@ import React, { Component } from "react";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css"; 
 import "slick-carousel/slick/slick-theme.css";
+import './SimpleSlider.css'
 
 export default class SimpleSlider extends Component {
   render() {
     const settings = {
+      className: "center",
       dots: true,
       infinite: true,
       speed: 500,
       slidesToShow: 1,
       slidesToScroll: 1,
       centerMode: true,
+      centerPadding: "60px",
       //adaptiveHeight: true,
       arrows: true,
       //autoplaySpeed: 1,
@@ -19,33 +22,22 @@ export default class SimpleSlider extends Component {
 
     };
     return (
-      <div>
+      <div className="SimpleSlider">
         <Slider {...settings}>
-          <div>
-            <h3>Image-1</h3>
+        <div>
+            <img src={"https://cdnb.artstation.com/p/assets/images/images/031/251/515/large/ash-thorp-019-the-shallows-deep-2560-001.jpg?1603079132"} width="70%" height="70%"  />
           </div>
           <div>
-            <h3>Image-2</h3>
+            <img src={"https://cdnb.artstation.com/p/assets/images/images/031/251/515/large/ash-thorp-019-the-shallows-deep-2560-001.jpg?1603079132"} width="70%" height="70%" />
           </div>
           <div>
-            <h3>Image-3</h3>
+            <img src={"https://cdnb.artstation.com/p/assets/images/images/031/251/515/large/ash-thorp-019-the-shallows-deep-2560-001.jpg?1603079132"} width="70%" height="70%" />
           </div>
           <div>
-            <h3>Image-4</h3>
+            <img src={"https://cdnb.artstation.com/p/assets/images/images/031/251/515/large/ash-thorp-019-the-shallows-deep-2560-001.jpg?1603079132"} width="70%" height="70%" />
           </div>
-          <div>
-            <h3>Image-5</h3>
-          </div>
-          <div>
-            <h3>Image-6</h3>
-          </div>
-        </Slider>
-      </div>
+  </Slider>
+  </div>
     );
-  }
 }
-
-var img = document.createElement('img')
-
-img.src = "https://media.cntraveler.com/photos/59ef91dd8d4f736d51415c2e/master/w_2667,h_2000,c_limit/7MileBeach-2013-HiRes.jpg"
-
+}
