2f5441841eb1f3ae9e020c08a4a4fd36469dc25f
Alondra Pereira <alondra.pereira@upr.edu>
2020-11-24 14:36:40 -0400

    Worked on customization of PlaceTable.

--- rumbapr/src/pages/placeCatalog.js
+++ rumbapr/src/pages/placeCatalog.js
@@ -5,7 +5,7 @@ import 'react-table/react-table.css';
 import './placeCatalog.css';
 import Header from "../components/header/Header";
 
-export default class App extends Component {
+export default class PlaceTable extends Component {
     constructor(props){
         super(props)
         this.state = {
@@ -15,7 +15,6 @@ export default class App extends Component {
     }
     async getPlacesData(){
         const res = await axios.get('http://localhost:5000/api/v2/place/all/')
-        console.log(res.data)
         this.setState({loading:false, places: res.data})
     }
     componentDidMount(){
@@ -25,37 +24,54 @@ export default class App extends Component {
         const columns = [
             {
                 Header: 'Name',
-                accessor: 'name'
+                id : 'name',
+                headerStyle: {color: '#00b7ea',  fontWeight: 'bold'},
+                accessor: 'name',
             }
             ,{
                 Header: 'Phone',
+                headerStyle: {color: '#00b7ea', fontWeight: 'bold'},
                 accessor: 'phone',
+                sortable: false,
             },
             {
                 Header: 'Email',
+                headerStyle: {color: '#00b7ea', fontWeight: 'bold'},
                 accessor: 'email',
+                sortable: false,
             },
             {
                 Header: 'City',
+                headerStyle: {color: '#00b7ea', fontWeight: 'bold'},
                 accessor: 'city',
+                sortable: false,
             },
             {
                 Header: 'Country',
+                headerStyle: {color: '#00b7ea', fontWeight: 'bold'},
                 accessor: 'country',
+                sortable: false,
             },
         ]
         return (
             <div style={{backgroundColor: '#303639'}}>
                 <Header />
                 <div className={"header"}>Places</div>
-                <ReactTable
+                    <ReactTable
                         style= {
-                            {   color: 'white',
+                            {
+                                color: 'white',
                                 marginLeft: '14px',
                                 marginRight: '29px',
-                                border: 'none'
+                                border: 'none',
                             }}
-                        className={'hoverable'}
+                        sorted={[
+                            {
+                                id: 'name',
+                                desc: false
+                            }]}
+                        minRows={0}
+                        defaultPageSize={10}
                         data={this.state.places}
                         columns={columns}
                     />
