164a2fd3a76a5cc942af0afe4d925c07750c2615
hackerrank <me@hackerrank.com>
2020-11-25 14:51:24 -0400

    Added base code for the sending of information.

--- rumbapr/src/components/HamburgerMenu.js
+++ rumbapr/src/components/HamburgerMenu.js
@@ -8,15 +8,27 @@ import { makeStyles } from '@material-ui/core/styles';
 import Slider from '@material-ui/core/Slider';
 
 
+
+// Width for sliders
 const useStyles = makeStyles({
   root: {
     width: 300,
   },
 });
 
+
+
 //Preffered distance slider
 function DistanceSlider() {
   const classes = useStyles();
+
+  //   useEffect(() => {
+//     const apiUrl = '';
+//     fetch(apiUrl)
+//         .then((response) => response.json())
+//         .then((databaseInfo) => setDataSet({locations: databaseInfo}))
+
+// }, []);
 
   return (
     <div className={classes.root}>
@@ -34,6 +46,8 @@ function DistanceSlider() {
   );
 }
 
+
+
 //Price range slider
 function RangeSlider() {
   const classes = useStyles();
@@ -43,6 +57,14 @@ function RangeSlider() {
     setValue(newValue);
   };
 
+  //   useEffect(() => {
+//     const apiUrl = '';
+//     fetch(apiUrl)
+//         .then((response) => response.json())
+//         .then((databaseInfo) => setDataSet({locations: databaseInfo}))
+
+// }, []);
+
   return (
     <div className={classes.root}>
       <Slider style = {{color: "#4CAF50"}}
@@ -50,68 +72,122 @@ function RangeSlider() {
         onChange={handleChange}
         valueLabelDisplay="auto"
         aria-labelledby="range-slider"
+        min={0}
+        max={100}
         //getAriaValueText={valuetext}
       />
     </div>
   );
 }
 
+
+
+// Function for the filter selection using hooks for data storage
+function Filters({data, setOrdered}) {
+  const [fil, setFil] = useState([]);
+  const optionFilter = [
+
+    // Ambience options
+    {id: 'Ambience' , value: 'Elegant', label:'Elegant'},
+    {id: 'Ambience' , value: 'Sophisticated', label:'Sophisticated'},
+    {id: 'Ambience' , value: 'Traditional', label:'Traditional'},
+    {id: 'Ambience' , value: 'Modern', label:'Modern'},
+    {id: 'Ambience' , value: 'Old fashioned', label:'Old fashioned'},
+    {id: 'Ambience' , value: 'Vintage', label:'Vintage'},
+    {id: 'Ambience' , value: 'Cultural', label:'Cultural'},
+    {id: 'Ambience' , value: 'Serene', label:'Serene'},
+    {id: 'Ambience' , value: 'Cozy', label:'Cozy'},
+    {id: 'Ambience' , value: 'Soothing', label:'Soothing'},
+    {id: 'Ambience' , value: 'Familiar', label:'Familiar'},
+    {id: 'Ambience' , value: 'Adult', label:'Adult'},
+    {id: 'Ambience' , value: 'Young', label:'Young'},
+    {id: 'Ambience' , value: 'Casual', label:'Casual'},
+    {id: 'Ambience' , value: 'Cultural', label:'Cultural'},
+
+    // Mood options
+    {id: 'Mood' , value: 'Curious', label:'Curious'},
+    {id: 'Mood' , value: 'Humorous', label:'Humorous'},
+    {id: 'Mood' , value: 'Calm', label:'Calm'},
+    {id: 'Mood' , value: 'Festive', label:'Festive'},
+    {id: 'Mood' , value: 'Happy', label:'Happy'},
+    {id: 'Mood' , value: 'Social', label:'Social'},
+    {id: 'Mood' , value: 'Adventurous', label:'Adventurous'},
+    {id: 'Mood' , value: 'Sad', label:'Sad'},
+    {id: 'Mood' , value: 'Tense', label:'Tense'},
+
+    // Category
+    {id: 'Category' , value: 'Nature', label:'Nature'},
+    {id: 'Category' , value: 'Food/Drinks', label:'Food/Drinks'},
+    {id: 'Category' , value: 'History', label:'History'},
+    {id: 'Category' , value: 'Entertainment ', label:'Entertainment '},
+    {id: 'Category' , value: 'Shopping', label:'Shopping'},
+    {id: 'Category' , value: 'Sports', label:'Sports'},
+    {id: 'Category' , value: 'Hotel/Motel/Lodges', label:'Hotel/Motel/Lodges'},
+
+
+  ];
+
+  //   useEffect(() => {
+//     const apiUrl = '';
+//     fetch(apiUrl)
+//         .then((response) => response.json())
+//         .then((databaseInfo) => setDataSet({locations: databaseInfo}))
+
+// }, []);
+  return (
+    <div>
+      <Select 
+        isMulti
+        options={optionFilter}
+        onChange={setFil}
+        noOptionsMessage={()=> 'No filter selected'}
+        onFocus
+        isSearchable
+        placeholder="Select filters"
+        className="mb-3"
+      />
+    </div>
+  )
+}
+
+
+//setState and builder for the place selection feature using hooks for data storage
+function Place({data, setOrdered}) {
+  const [place, setPlace] = useState([]);
+  const optionFilter = [
+    {value: 'Any', label:'Any'},
+    {value: 'Food', label:'Food'},
+    {value: 'Hotel', label:'Hotel'},
+    {value: 'Beach', label:'Beach'},
+    {value: 'Attraction', label:'Attraction'}
+
+  ];
+
+//   useEffect(() => {
+//     const apiUrl = '';
+//     fetch(apiUrl)
+//         .then((response) => response.json())
+//         .then((databaseInfo) => setDataSet({locations: databaseInfo}))
+
+// }, []);
+
+  return (
+    <div>
+      <Select 
+        options={optionFilter}
+        onChange={setPlace}
+        noOptionsMessage={()=> 'Any'}
+        onFocus
+        isSearchable
+        placeholder="Any"
+        className="mb-3"
+      />
+    </div>
+  )
+}
+
+//Main function
 export function HamburgerMenu () {
-
-
-  // Function for the filter selection using hooks for data storage
-  function Filters({data, setOrdered}) {
-    const [fil, setFil] = useState([]);
-    const optionFilter = [
-      {value: 'Elegant', label:'Elegant'},
-      {value: 'Sophisticated', label:'Sophisticated'},
-      {value: 'Traditional', label:'Traditional'},
-      {value: 'Modern', label:'Modern'},
-      {value: 'Old fashioned', label:'Old fashioned'},
-      {value: 'Vintage', label:'Vintage'},
-      {value: 'Cultural', label:'Cultural'},
-      {value: 'Serene', label:'Serene'},
-      {value: 'Cozy', label:'Cozy'},
-      {value: 'Soothing', label:'Soothing'},
-      {value: 'Familiar', label:'Familiar'},
-      {value: 'Adult', label:'Adult'},
-      {value: 'Young', label:'Young'},
-      {value: 'Casual', label:'Casual'},
-      {value: 'Cultural', label:'Cultural'},
-      {value: 'Curious', label:'Curious'},
-      {value: 'Humorous', label:'Humorous'},
-      {value: 'Calm', label:'Calm'},
-      {value: 'Festive', label:'Festive'},
-      {value: 'Happy', label:'Happy'},
-      {value: 'Social', label:'Social'},
-      {value: 'Adventurous', label:'Adventurous'},
-      {value: 'Sad', label:'Sad'},
-      {value: 'Tense', label:'Tense'},
-      {value: 'Nature', label:'Nature'},
-      {value: 'Food/Drinks', label:'Food/Drinks'},
-      {value: 'History', label:'History'},
-      {value: 'Entertainment ', label:'Entertainment '},
-      {value: 'Shopping', label:'Shopping'},
-      {value: 'Sports', label:'Sports'},
-      {value: 'Hotel/Motel/Lodges', label:'Hotel/Motel/Lodges'},
-
-
-    ];
-    return (
-      <div>
-        <Select 
-          isMulti
-          options={optionFilter}
-          onChange={setFil}
-          noOptionsMessage={()=> 'No filter selected'}
-          onFocus
-          isSearchable
-          placeholder="Select filters"
-          className="mb-3"
-        />
-      </div>
-    )
-  }
 
   // HamburgerMenu
     return(
@@ -126,17 +202,12 @@ export function HamburgerMenu () {
                 <ul>
                   <Container>
                     <form color= "white">
-                      <div className="myPlace">
 
-                        {/* Place of chice */}
+
+                      {/* Place of chice */}
+                      <div className="myPlace">
                         <p  style = {{color: "#fff"}}>Place</p>
-                        <select id="myPlace" on change="place()">
-                          <option>Any</option>
-                          <option>Food</option>
-                          <option>Hotel</option>
-                          <option>Beach</option>
-                          <option>Attraction</option>
-                        </select>
+                        <Place/>
                       </div>
 
                       {/* Filters */}
