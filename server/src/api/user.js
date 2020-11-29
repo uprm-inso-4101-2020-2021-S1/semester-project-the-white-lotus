4fa6b4dacfc3883c378eb8cdf283bc6ebd2cf888
Alondra Pereira <alondra.pereira@upr.edu>
2020-11-29 18:56:53 -0400

    dummy fix

--- server/src/api/user.js
+++ server/src/api/user.js
@@ -1,9 +1,10 @@
-const express = require("express");
+const express = require('express');
+
 const router = express.Router();
-const to = require('../utils').to;
+const { to } = require('../utils');
 
 // User registration authorization
-const AuthController = require('../../controllers/AuthController')
+const AuthController = require('../../controllers/AuthController');
 
 // User model
 const User = require('../../models/User');
@@ -11,8 +12,8 @@ const User = require('../../models/User');
 /**
  * Getting All Users
  * Simple function that returns all the current users
- * that are within the Data Base. 
- * 
+ * that are within the Data Base.
+ *
  * The proper way to call this function is by making a request
  * through:
  * host/api/v2/user/all
@@ -32,20 +33,20 @@ const User = require('../../models/User');
  *          description: An internal server error occurred
  */
 router.get('/all', async (req, res) => {
-  const users = await User.find()
-  res.send(users)
-})
+  const users = await User.find();
+  res.send(users);
+});
 
 /**
  * Get User by ID (individual)
  * This function returns a specified user by ID. If
  * the ID does not exist in the Data Base then the
  * function will return a 'User not found.' message.
- * 
+ *
  * The proper way to call this function is by making a request
  * through:
  * host/api/v2/user/:id
- * 
+ *
  * Where :id is replaced by the actual ID of the user
  * Example: host/api/v2/user/5f73377e556c7bf80113cfe4
  */
@@ -73,24 +74,23 @@ router.get('/all', async (req, res) => {
  *        description: An internal server error ocurred
  */
 router.get('/:id', async (req, res, next) => {
-  const [err, user] = await to(User.findOne({_id: req.params.id}))
-
+  const [err, user] = await to(User.findOne({ _id: req.params.id }));
   // If an error ocurred, throw to handler
-  if(err) {
-    return next(err)
+  if (err) {
+    return next(err);
   }
 
   // If user w/ id is not found, return 'not found'
-  if(!user) {
-    res.status(404)
+  if (!user) {
+    res.status(404);
     return res.send({
       msg: 'User not found.'
-    })
+    });
   }
 
   // Found user
-  res.send(user)
-})
+  res.send(user);
+});
 
 /**
  * User Registration
@@ -100,8 +100,8 @@ router.get('/:id', async (req, res, next) => {
  * The proper way to call this function is by making a request
  * through:
  * host/api/v2/user/register
- * 
- * For detailed information go to 
+ *
+ * For detailed information go to
  * repo/server/controllers/AuthController
  */
 /**
@@ -125,7 +125,7 @@ router.get('/:id', async (req, res, next) => {
  *      '500':
  *        description: An internal server error occurred
  */
-router.post('/register', AuthController.register)
+router.post('/register', AuthController.register);
 
 /**
  * User Login
@@ -136,7 +136,7 @@ router.post('/register', AuthController.register)
  * through:
  * host/api/v2/user/login
  *
- * For detailed information go to 
+ * For detailed information go to
  * repo/server/controllers/AuthController
  */
 /**
@@ -176,19 +176,19 @@ router.post('/register', AuthController.register)
  *      '500':
  *        description: An internal server error ocurred
  */
-router.post('/login', AuthController.login)
+router.post('/login', AuthController.login);
 
 /**
  * Updating User's attributes
  * The function takes care of updating the attributes
  * of a single user by checking which parameters were
- * given first and then assigning it to the ones 
+ * given first and then assigning it to the ones
  * currently saved on the Data Base.
- * 
+ *
  * The proper way to call this function is by making a request
  * through:
  * host/api/v2/user/update/:id
- * 
+ *
  * Where :id is replaced the actual ID of the user that is
  * going to get the attributes update.
  * Example:
@@ -200,9 +200,9 @@ router.post('/login', AuthController.login)
  *    "comment": "Best Burger I've had in years!"
  *  }
  * }
- * 
+ *
  * Note that to update the comment attribute is different. Refer
- * to the User model under 
+ * to the User model under
  * repo/server/models/User
  * for more information.
  */
@@ -246,54 +246,54 @@ router.post('/login', AuthController.login)
  *        description: An internal server error ocurred
  */
 router.patch('/update/:id', async (req, res, next) => {
-  const [err, user] = await to(User.findOne({_id: req.params.id}))
+  const [err, user] = await to(User.findOne({ _id: req.params.id }));
 
   // If an error ocurred, throw to handler
-  if(err) {
-    return next(err)
+  if (err) {
+    return next(err);
   }
 
   // If user w/ id is not found, return 'not found'
-  if(!user){
-    res.status(404)
+  if (!user) {
+    res.status(404);
     return res.send({
       msg: 'User not found'
-    })
+    });
   }
 
   /* Update attributes (not allowing email change for now since
      it is unique) */
-  if(req.body.name){
-    user.name = req.body.name
+  if (req.body.name) {
+    user.name = req.body.name;
   }
-  if(req.body.address){
-    user.address = req.body.address
+  if (req.body.address) {
+    user.address = req.body.address;
   }
-  if(req.body.phone){
-    user.phone = req.body.phone
+  if (req.body.phone) {
+    user.phone = req.body.phone;
   }
-  if(req.body.ishost){
-    user.ishost = req.body.ishost
+  if (req.body.ishost) {
+    user.ishost = req.body.ishost;
   }
-  if(req.body.comment){
+  if (req.body.comment) {
     user.commenthistory.push({
       place: req.body.comment.place,
       date: String(new Date()),
       comment: req.body.comment.comment
-    })
+    });
   }
-  if(req.body.hashtags){
-    if(!user.hashtags.includes(req.body.hashtags)){
-      user.hashtags.push(req.body.hashtags)
+  if (req.body.hashtags) {
+    if (!user.hashtags.includes(req.body.hashtags)) {
+      user.hashtags.push(req.body.hashtags);
     }
   }
 
-  //TODO: Update password
+  // TODO: Update password
 
   // Save and send updated user
-  await user.save()
-  res.send(user)
-})
+  await user.save();
+  res.send(user);
+});
 
 /**
  * Delete User
@@ -302,11 +302,11 @@ router.patch('/update/:id', async (req, res, next) => {
  * given, in this case the ID of such user. Returns whether the
  * user was not found or if the user was successfully removed
  * from the Data Base.
- * 
+ *
  * The proper way to call this function is by making a request
  * through:
  * host/api/v2/user/delete/:id
- * 
+ *
  * Where :id is replaced the actual ID of the user that is
  * going to get the attributes update.
  * Example: host/api/v2/user/5f73377e556c7bf80113cfe4
@@ -337,26 +337,26 @@ router.patch('/update/:id', async (req, res, next) => {
 router.delete('/delete/:id', async (req, res, next) => {
   // The deleteOne() method returns an object containing three fields.
   // n – number of matched documents
-  const [err, result] = await to(User.deleteOne({_id: req.params.id}))
+  const [err, result] = await to(User.deleteOne({ _id: req.params.id }));
 
-  // If an error ocurred 
-  if(err) {
-    return next(err)
+  // If an error ocurred
+  if (err) {
+    return next(err);
   }
 
   // If result.n is not zero, the user was not found.
-  if(!result.n){
-    res.status(404)
+  if (!result.n) {
+    res.status(404);
     return res.send({
       msg: 'User was not found.'
-    })
+    });
   }
 
   // User was deleted successfully, send result
   res.send({
     result,
     msg: `User ${req.params.id} deleted successfully.`
-  })
-})
+  });
+});
 
-module.exports = router
+module.exports = router;
